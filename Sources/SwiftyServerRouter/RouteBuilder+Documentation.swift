52f70ff415f97f4254e12ba76cc29d27b5dec9ba
Simon Strandgaard <neoneye@gmail.com>
2018-06-19 23:57:58 +0100

    Imported core files from the VisualOpus project

---
+++ Sources/SwiftyServerRouter/RouteBuilder+Documentation.swift
@@ -0,0 +1,72 @@
+// MIT license. Copyright (c) 2018 SwiftyServerRouter. All rights reserved.
+#if canImport(PerfectHTTP)
+import PerfectHTTP
+#endif
+
+class RouteBuilder_Documentation: RouteBuilder {
+	private var stack = [String]()
+	var items = [EndpointItem]()
+
+	func scope(_ item: String, scopeInner: () -> Void) {
+		push(item)
+		scopeInner()
+		pop()
+	}
+
+	private func push(_ item: String) {
+		stack.append(item)
+	}
+
+	private func pop() {
+		guard stack.popLast() != nil else {
+			fatalError("Expected same number of push and pops, but more pop()s are invoked")
+		}
+	}
+
+	#if canImport(PerfectHTTP)
+	func perfect_endpoint(method: PerfectHTTP.HTTPMethod, route: String, purpose: String, data: [String:Any], handler: @escaping Perfect_ReturnsRequestHandlerGivenData) {
+		let uri: String = stack.joined() + route
+
+		let item = EndpointItem(
+			method: method.description.lowercased(),
+			route: uri,
+			purpose: purpose,
+			insecure_noAuthentication: true,
+			className: "simple"
+		)
+		self.items.append(item)
+	}
+	#endif
+
+	func endpoint<T: Endpoint>(method: Endpoint_HTTPMethod, route: String, handlerType: T.Type) {
+		let uri: String = stack.joined() + route
+		let className = String(describing: handlerType)
+		let handler: Endpoint = handlerType.init()
+		let purpose: String = handler.purpose
+		let insecure_noAuthentication: Bool = handler is Insecure_NoAuthentication
+		let item = EndpointItem(
+			method: method.description.lowercased(),
+			route: uri,
+			purpose: purpose,
+			insecure_noAuthentication: insecure_noAuthentication,
+			className: className
+		)
+		self.items.append(item)
+	}
+}
+
+class EndpointItem {
+	let method: String
+	let route: String
+	let purpose: String
+	let insecure_noAuthentication: Bool
+	let className: String
+
+	init(method: String, route: String, purpose: String, insecure_noAuthentication: Bool, className: String) {
+		self.method = method
+		self.route = route
+		self.purpose = purpose
+		self.insecure_noAuthentication = insecure_noAuthentication
+		self.className = className
+	}
+}
---
+++ Sources/SwiftyServerRouter/RouteBuilder+Perfect.swift
@@ -0,0 +1,123 @@
+// MIT license. Copyright (c) 2018 SwiftyServerRouter. All rights reserved.
+#if canImport(PerfectHTTP)
+import PerfectHTTP
+
+
+class RouteBuilder_PerfectRoutes: RouteBuilder {
+	private var stack = [String]()
+	var routes = PerfectHTTP.Routes()
+
+	func scope(_ item: String, scopeInner: () -> Void) {
+		push(item)
+		scopeInner()
+		pop()
+	}
+
+	private func push(_ item: String) {
+		stack.append(item)
+	}
+
+	private func pop() {
+		guard stack.popLast() != nil else {
+			fatalError("Expected same number of push and pops, but more pop()s are invoked")
+		}
+	}
+
+	func endpoint<T: Endpoint>(method: Endpoint_HTTPMethod, route: String, handlerType: T.Type) {
+		let uri: String = stack.joined() + route
+		let perfectHandler = exceptionHandler() { (request: HTTPRequest, response: HTTPResponse) in
+			let context = HandlerContext(request: request, response: response)
+			let handler: Endpoint = handlerType.init()
+			try handler.handler(context: context)
+		}
+		routes.add(method: method, uri: uri, handler: perfectHandler)
+	}
+
+	func perfect_endpoint(method: PerfectHTTP.HTTPMethod, route: String, purpose: String, data: [String:Any], handler: @escaping Perfect_ReturnsRequestHandlerGivenData) {
+		let uri: String = stack.joined() + route
+		guard let resolvedHandler = try? handler(data) else {
+			log.error("endpoint did not return a handler")
+			return
+		}
+		routes.add(method: method, uri: uri, handler: resolvedHandler)
+	}
+}
+
+
+/// Build a dictionary for use with `HTTPServerExConfig.swift`
+/// That is initialized like this: `try HTTPServer.launch(configurationData: confData)`
+class RouteBuilder_PerfectDictionary: RouteBuilder {
+	private var stack = [String]()
+	var routes = [[String: Any]]()
+
+	func scope(_ item: String, scopeInner: () -> Void) {
+		push(item)
+		scopeInner()
+		pop()
+	}
+
+	private func push(_ item: String) {
+		stack.append(item)
+	}
+
+	private func pop() {
+		guard stack.popLast() != nil else {
+			fatalError("Expected same number of push and pops, but more pop()s are invoked")
+		}
+	}
+
+	func endpoint<T: Endpoint>(method: Endpoint_HTTPMethod, route: String, handlerType: T.Type) {
+		let uri: String = stack.joined() + route
+		let perfectHandler = exceptionHandler() { (request: HTTPRequest, response: HTTPResponse) in
+			let context = HandlerContext(request: request, response: response)
+			let handler: Endpoint = handlerType.init()
+			try handler.handler(context: context)
+		}
+		let methodName = method.description.lowercased()
+		routes.append(["method":methodName, "uri":uri, "handler":perfectHandler])
+	}
+
+	func perfect_endpoint(method: PerfectHTTP.HTTPMethod, route: String, purpose: String, data: [String:Any], handler: @escaping Perfect_ReturnsRequestHandlerGivenData) {
+		let uri: String = stack.joined() + route
+		let methodName = method.description.lowercased()
+		var data2 = data
+		data2["method"] = methodName
+		data2["uri"] = uri
+		data2["handler"] = handler
+		routes.append(data2)
+	}
+}
+
+fileprivate typealias ThrowingRequestHandler = (HTTPRequest, HTTPResponse) throws -> ()
+
+fileprivate func exceptionHandler(_ handler: @escaping ThrowingRequestHandler) -> RequestHandler {
+	let result: (HTTPRequest, HTTPResponse) -> () = { (request, response) in
+		do {
+			try handler(request, response)
+		} catch let NetworkError.custom(message, status) {
+			var dict = [String: Any]()
+			dict["uri"] = request.uri
+			dict["status"] = status.code
+			dict["description"] = status.description
+			dict["message"] = message
+			let jsonString: String? = try? dict.jsonEncodedString()
+			response.status = status
+			response.setHeader(.contentType, value: "application/json")
+			response.setBody(string: jsonString ?? "{}")
+			response.completed()
+		} catch {
+			var dict = [String: Any]()
+			dict["uri"] = request.uri
+			dict["status"] = 500
+			dict["message"] = "Unhandled exception: \(error)"
+			let jsonString: String? = try? dict.jsonEncodedString()
+			response.status = .internalServerError
+			response.setHeader(.contentType, value: ContentType.application_json.rawValue)
+			response.setBody(string: jsonString ?? "{}")
+			response.completed()
+		}
+	}
+	return result
+}
+
+#endif
